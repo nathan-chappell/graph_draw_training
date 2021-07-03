858e0e71cd15b7b839dd9fa849956b0765a45fb7
Nathan Chappell <nathan.s.chappell@gmail.com>
2021-07-03 10:54:32 +0200

    refactor functions

--- frontend/scripts/gen_index.py
+++ frontend/scripts/gen_index.py
@@ -10,23 +10,39 @@ sys.path.append(load_deps_dir)
 
 from load_deps import JsDepGraph, get_dependency_order
 
-index_path = '../js/index.js'
-root_dir = '../js'
+def get_deps(index_path, root_dir):
+    index = Path(index_path)
+    depGraph = JsDepGraph(root_dir=root_dir)
+    depGraph.to_dot()
+    dep_order = get_dependency_order(depGraph)
+    pprint(dep_order)
+    deps = [str(Path(p).relative_to(Path('..').resolve())) + '.js' for p in dep_order]
+    return deps
 
-index = Path(index_path)
-depGraph = JsDepGraph(root_dir=root_dir)
-depGraph.to_dot()
-dep_order = get_dependency_order(depGraph)
-pprint(dep_order)
-deps = [str(Path(p).relative_to(Path('..').resolve())) + '.js' for p in dep_order]
+def get_rendered_template(template_dir, template_file, template_args):
+    template_contents = None
+    with open(f'{template_dir}/{template_file}') as f:
+        template_contents = f.read()
+    template = jinja2.Template(template_contents)
+    rendered = template.render(**template_args)
+    print(rendered)
+    return rendered
 
-template_dir = 'templates'
-index_template_file = 'index.html'
+if __name__ == '__main__':
 
-template_contents = None
-with open(f'{template_dir}/{index_template_file}') as f:
-    template_contents = f.read()
-template = jinja2.Template(template_contents)
+    index_path = '../js/index.js'
+    root_dir = '../js'
+    deps = get_deps(index_path, root_dir)
 
-rendered = template.render(deps=deps)
-print(rendered)
+    template_dir = 'templates'
+    index_template_file = 'index.html'
+    template_args = {'deps': deps}
+    rendered_template = get_rendered_template(
+        template_dir,
+        index_template_file, 
+        template_args
+    )
+
+    out_file = '../index.html'
+    with open(out_file, 'w') as f:
+        f.write(rendered_template)
