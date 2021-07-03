c70f14c47a2c9821a5db6ea7a3d159b5d6b41d22
Nathan Chappell <nathan.s.chappell@gmail.com>
2021-07-03 14:33:43 +0200

    server get helloWorld

--- backend/std_server/server.py
+++ backend/std_server/server.py
@@ -1,9 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200,'hello world')
+        self.send_header('access-control-allow-origin','*')
+        self.send_header('content-type','application/json')
         self.end_headers()
+        x = {'foo': 'bar'}
+        data = json.dumps(x)
+        self.wfile.write(bytes(data,'ascii'))
 
 if __name__ == '__main__':
     server = HTTPServer(('localhost',8888), RequestHandler)
