f13db14d826a87b7b5d69da2fd8b6947d3e566e6
Nathan Chappell <nathan.s.chappell@gmail.com>
2021-07-03 10:49:21 +0200

    refactor variables

--- frontend/scripts/gen_index.py
+++ frontend/scripts/gen_index.py
@@ -3,20 +3,28 @@ from pathlib import Path
 
 import jinja2
 
+load_deps_dir = '../../'
+
 import sys
-sys.path.append('../../')
+sys.path.append(load_deps_dir)
 
 from load_deps import JsDepGraph, get_dependency_order
 
-index = Path('../js/index.js')
-depGraph = JsDepGraph(root_dir='../js')
+index_path = '../js/index.js'
+root_dir = '../js'
+
+index = Path(index_path)
+depGraph = JsDepGraph(root_dir=root_dir)
 depGraph.to_dot()
 dep_order = get_dependency_order(depGraph)
 pprint(dep_order)
 deps = [str(Path(p).relative_to(Path('..').resolve())) + '.js' for p in dep_order]
 
+template_dir = 'templates'
+index_template_file = 'index.html'
+
 template_contents = None
-with open('templates/index.html') as f:
+with open(f'{template_dir}/{index_template_file}') as f:
     template_contents = f.read()
 template = jinja2.Template(template_contents)
 
