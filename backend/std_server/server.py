6bf6670af42cb0056dab8e799bd59ede177553ed
Nathan Chappell <nathan.s.chappell@gmail.com>
2021-07-03 15:06:52 +0200

    cors - initial

--- backend/std_server/server.py
+++ backend/std_server/server.py
@@ -1,15 +1,34 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
+def cors(requestHandler):
+    requestHandler.send_header('access-control-allow-origin','*')
+    requestHandler.send_header('access-control-allow-methods','GET, POST, OPTIONS')
+    requestHandler.send_header('access-control-allow-headers','content-type')
+
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200,'hello world')
-        self.send_header('access-control-allow-origin','*')
+        cors(self)
         self.send_header('content-type','application/json')
         self.end_headers()
         x = {'foo': 'bar'}
         data = json.dumps(x)
         self.wfile.write(bytes(data,'ascii'))
+
+    def do_POST(self):
+        self.send_response(200,'hello world')
+        cors(self)
+        self.send_header('content-type','application/json')
+        self.end_headers()
+        x = {'foo': 'bar'}
+        data = json.dumps(x)
+        self.wfile.write(bytes(data,'ascii'))
+
+    def do_OPTIONS(self):
+        self.send_response(200,'hello world')
+        cors(self)
+        self.end_headers()
 
 if __name__ == '__main__':
     server = HTTPServer(('localhost',8888), RequestHandler)
